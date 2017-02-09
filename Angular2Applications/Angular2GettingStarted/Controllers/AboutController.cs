0700b488e7bdcacc29a992f42f3ed408f0366f59
Addisu Desta <Addisu Desta>
2017-02-09 14:36:28 -0500

    initial Pluralsight - Scott excercise

---
+++ Angular2Applications/Angular2GettingStarted/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular2GettingStarted.Controllers
+{
+    [Route("company/[controller]")]
+    public class AboutController
+    {
+        [Route("")]
+        public string Phone()
+        {
+            return "+1-555-123-4567";
+        }
+
+        [Route("[action]")]
+        public string Country()
+        {
+            return "Canada";
+        }
+    }
+}
---
+++ Angular2Applications/Angular2GettingStarted/Startup.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Angular2GettingStarted.Services;
+using System;
+using Microsoft.AspNetCore.Routing;
+
+namespace Angular2GettingStarted
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json", false, true);
+
+            Configuration = builder.Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //there are default registrations 
+            services.AddMvc();
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            IGreeter greeter)
+        {
+            loggerFactory.AddConsole();
+
+            
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            // app.UseDefaultFiles();
+            // app.UseStaticFiles();
+            // Equivalent to the following 
+            app.UseFileServer();
+
+            app.UseMvc(configureRoute);
+
+            app.Run(async (context) =>
+            {
+                var greeting = greeter.GetGreeting();
+
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+
+        private void configureRoute(IRouteBuilder routeBuilder)
+        {
+            // /
+            // Home/Index/1
+
+            routeBuilder.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
